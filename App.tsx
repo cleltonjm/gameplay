c283587031de29f78044a47c142e5487b06eef2d
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2021-11-11 19:13:15 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
@@ -19,6 +20,13 @@ export default function App() {
   }
 
   return (
+    <>
+    <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     <SignIn />
+    </>
   );
 }
